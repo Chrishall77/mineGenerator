7caff554fe2adb6913d3b05f85122d37d6dc7dbb
Chris Hall <chrishallhome@gmail.com>
2020-01-09 14:22:00 +0000

    Generates grid

--- mine.js
+++ mine.js
@@ -1,11 +1,38 @@
 ((d)=> {
-    //initialise rows and columns
+    //initialise DOM elements
     let rows = d.getElementById("rows");
     let cols = d.getElementById("cols");
+    let mines = d.getElementById("mines");
+    let gridContainer = d.getElementById("gridContainer");
     let submit = d.getElementById("submit");
+    
 
     submit.addEventListener("click", ()=> {
-        console.log("Rows " + rows.value + " Cols " + cols.value);
+        let currentRows = rows.value;
+        let currentCols = cols.value;
+        let currentMines = mines.value;
+        generateGrid(currentRows, currentCols);
     })
-    
+
+    function generateGrid(r, c) {
+        clearGrid();
+        for(var i = 0; i < r; i++){ 
+          var row = document.createElement("div");  
+          for(var x = 1; x <= c; x++){ 
+              var cell = document.createElement("div"); 
+              cell.className = "grid"; 
+              cell.innerText = (i * r) + c;
+              row.appendChild(cell); 
+          } 
+          gridContainer.appendChild(row); 
+        }   
+    };
+
+    function clearGrid() {
+        let element = document.getElementById("gridContainer");
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+    }
+
 })(document)
